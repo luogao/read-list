b7e66753fca4bd458f638110efc6d6e151083934
Roy <luogao_lg@sina.com>
2018-03-29 20:08:41 +0800

    完成新建功能

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import AV from 'leancloud-storage'
 
 class ReadListManager {
   constructor () {
@@ -11,6 +12,54 @@ class ReadListManager {
   }
   getLinkInfo (link) {
     return this.$http.post('/getinfo', { link })
+  }
+  createTag (tag) {
+    const Tag = AV.Object.extend('Tag')
+    const _tag = new Tag()
+    _tag.set(tag)
+    return _tag.save()
+  }
+  getTags () {
+    const tagQuery = new AV.Query('Tag')
+    return tagQuery.find()
+  }
+  createCategory (host) {
+    let _host = {
+      name: host.host_name,
+      display_name: host.host_display_name,
+      protocol: host.host_protocol,
+      favicon: host.favicon === -1 ? '' : host.favicon
+    }
+    const Category = AV.Object.extend('Category')
+    const _category = new Category()
+    _category.set(_host)
+    return _category.save()
+  }
+  createReadTagMap (tags, read) {
+    return tags.map(el => {
+      const _readTagMap = new AV.Object('ReadTagMap')
+      _readTagMap.set('read', read)
+      _readTagMap.set('tag', AV.Object.createWithoutData('Tag', el))
+      return _readTagMap.save()
+    })
+  }
+  async createRead (read, host) {
+    const { readTags, ...readInfo } = read
+    let { hostId, ...hostInfo } = host
+    const Read = AV.Object.extend('Read')
+    const _read = new Read()
+    if (!hostId) {
+      const newCategory = await this.createCategory(hostInfo)
+      hostId = newCategory.get('objectId')
+    }
+    const _category = AV.Object.createWithoutData('Category', hostId)
+    _read.set(readInfo)
+    _read.set('_category', _category)
+    const newRead = await _read.save()
+    if (readTags.length > 0) {
+      await this.createReadTagMap(readTags, newRead)
+    }
+    return newRead
   }
 }
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,91 +14,152 @@
         </li>
       </template>
     </ul>
-    <Button type="primary" @click="createModel = true">添加</Button>
+    <Button type="primary" @click="openCreateModel">添加</Button>
       <Modal
         @on-visible-change="modelVisibleChange"
         v-model="createModel"
-        title="新建"
-        @on-ok="ok"
-        @on-cancel="cancel">
-        <Form :model="formItem" :label-width="80" ref="createForm">
-          <FormItem label="标题" v-if="formItem.read_title">
+        title="新建">
+        <Form :model="formItem" :label-width="80" ref="createForm" label-position="left" :rules="ruleValidate">
+          <FormItem label="链接" prop="link">
+            <i-Input v-model="formItem.link" placeholder="请输入链接">
+              <Button :loading="getInfoLoading" slot="append" @click="getLinkInfo" icon="ios-search"></Button>
+            </i-Input>
+          </FormItem>
+          <FormItem label="标题" prop="read_title">
             <Input v-model="formItem.read_title"/>
           </FormItem>
-          <FormItem label="链接">
-            <Input v-model="formItem.link" placeholder="请输入链接">
-              <Button :loading="getInfoLoading" slot="append" @click="getLinkInfo" icon="ios-search"></Button>
-            </Input>
+          <FormItem label="标签" prop="readTags">
+            <Row type="flex" justify="space-between">
+                <template v-if="!createNewTag">
+                  <i-Col span="21">
+                    <Select v-model="formItem.readTags" multiple>
+                      <Option :value="item.objectId" v-for=" item in tags" :key="item.objectId">{{item.name}}</Option>
+                    </Select>
+                  </i-Col>
+                  <i-Col span="2">
+                    <Button shape="circle" @click="createNewTag = true" icon="plus-round"></Button>
+                  </i-Col>
+                </template>
+                <template v-else>
+                  <i-Col span="19">
+                    <Input v-model="newTag" placeholder="请输入标签名"/>
+                  </i-Col>
+                  <i-Col span="4">
+                    <Button :loading="createNewTagLoading" shape="circle" @click="handleCreateNewTag" icon="checkmark-round"></Button>
+                    <Button type="error" shape="circle" @click="cancelCreateTag" icon="close-round"></Button>
+                  </i-Col>
+                </template>
+            </Row>
           </FormItem>
-          <FormItem label="类型">
-              <Select v-model="formItem.type">
-                  <Option value="beijing">New York</Option>
-                  <Option value="shanghai">London</Option>
-                  <Option value="shenzhen">Sydney</Option>
-              </Select>
-          </FormItem>
-          <FormItem label="急需阅读">
+          <FormItem label="急需阅读" prop="isUrgent">
             <Checkbox v-model="formItem.isUrgent"></Checkbox>
           </FormItem>
-          <FormItem label="阅读状态">
-            <i-switch v-model="formItem.switch" size="large">
+          <FormItem label="阅读状态" prop="isRead">
+            <i-switch v-model="formItem.isRead" size="large">
                 <span slot="open">已读</span>
                 <span slot="close">未读</span>
             </i-switch>
           </FormItem>
         </Form>
+        <div slot="footer">
+          <Button :disabled="createModelLoading" @click="createModel = false">取消</Button>
+          <Button :loading="createModelLoading" @click="createRead" type="primary">提交</Button>
+        </div>
       </Modal>
   </div>
 </template>
 
 <script>
 import ReadListManager from '../api/index.js'
-import { bookmarksData } from '../api/bookmarks_data.js'
+// import { bookmarksData } from '../api/bookmarks_data.js'
 import SingleRead from '@/components/SingleRead'
 export default {
   name: 'Home',
   data () {
     return {
       createModel: false,
-      list: [],
       getInfoLoading: false,
-      linkFullInfo: null,
+      createNewTagLoading: false,
+      createNewTag: false,
+      createModelLoading: false,
+      list: [],
+      tags: [],
+      hostFullInfo: null,
+      newTag: '',
       formItem: {
         link: '',
-        type: '',
+        readTags: [],
         read_title: '',
         isRead: false,
         isUrgent: false
+      },
+      ruleValidate: {
+        link: [
+          { required: true, message: '链接不能为空', trigger: 'blur' }
+        ],
+        read_title: [
+          { required: true, message: '标题不能为空', trigger: 'blur' }
+        ]
       }
     }
   },
-  beforeMount () {
-    console.log(bookmarksData)
-  },
+  beforeMount () {},
   methods: {
     async getData (target) {
       // const data = await ReadListManager.getLinkInfo()
       // this.$set(this, target, data.data.list)
     },
-    ok () {},
-    cancel () {},
+    openCreateModel () {
+      this.getTagsData().then(this.createModel = true)
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+      this.newTag = ''
+      this.createNewTag = false
+    },
+    cancelCreateTag () {
+      this.newTag = ''
+      this.createNewTag = false
+    },
+    modelVisibleChange (status) {
+      if (!status) {
+        this.handleReset('createForm')
+      }
+    },
     async getLinkInfo () {
       const link = this.formItem.link
       if (link) {
         this.getInfoLoading = true
         const data = await ReadListManager.getLinkInfo(link)
         this.getInfoLoading = false
-        this.linkFullInfo = data.data
+        this.hostFullInfo = data.data
         this.formItem.read_title = data.data.read_title
       }
     },
-    handleReset (name) {
-      this.$refs[name].resetFields()
-    },
-    modelVisibleChange (status) {
-      if (!status) {
-        this.handleReset('createForm')
+    async handleCreateNewTag () {
+      if (this.newTag) {
+        let newTag = {
+          name: this.newTag
+        }
+        this.createNewTagLoading = true
+        await ReadListManager.createTag(newTag)
+        this.getTagsData().then(this.createNewTagLoading = false)
       }
+    },
+    async getTagsData () {
+      const tagData = await ReadListManager.getTags()
+      this.tags = tagData.map(el => {
+        return {
+          name: el.get('name'),
+          objectId: el.get('objectId')
+        }
+      })
+    },
+    async createRead () {
+      this.createModelLoading = true
+      await ReadListManager.createRead(this.formItem, this.hostFullInfo)
+      this.createModelLoading = false
+      this.$nextTick(() => { this.createModel = false })
     }
   },
   components: {
@@ -121,9 +182,6 @@ ul {
   padding: 0;
   margin: 0;
   overflow: hidden;
-}
-li {
-  margin: 10px 0;
 }
 a {
   color: #42b983;
